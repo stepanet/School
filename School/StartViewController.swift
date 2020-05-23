12dd7f2506630420f193894e1c9f7e65d3468c5e
stepanet <stepanet@gmail.com>
2020-05-23 21:36:19 +0300

    start project

---
+++ School/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  School
+//
+//  Created by Jack Sp@rroW on 23.05.2020.
+//  Copyright © 2020 Jack Sp@rroW. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
